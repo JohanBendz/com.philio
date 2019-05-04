47b64b6ff47ac46dc2e28123555d27a617ec271c
R Mengers <caseda08@hotmail.com>
2019-05-04 22:13:34 +0200

    update PAN08

--- drivers/PAN08-1/device.js
+++ drivers/PAN08-1/device.js
@@ -4,7 +4,6 @@ const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
 class PAN08 extends ZwaveDevice {
 	onMeshInit() {
-		this.registerCapability('windowcoverings_state', 'SWITCH_BINARY');
 		this.registerCapability('dim', 'SWITCH_MULTILEVEL');
 		this.registerCapability('measure_power', 'METER');
 		this.registerCapability('meter_power', 'METER');
